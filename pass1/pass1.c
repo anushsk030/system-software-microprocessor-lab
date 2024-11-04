9de0dc1e317af9314bd7fb6898dab54a462b36fb
ANUSH S KUMAR <145442617+anushsk030@users.noreply.github.com>
2024-11-05 01:07:19 +0530

    Update pass1.c

--- pass1/pass1.c
+++ pass1/pass1.c
@@ -1,85 +1,91 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
+ #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-void main(){
-   FILE *source ,*optab,*intermediate,*symtab,*length;
-   source=fopen("source.txt","r");
-   optab=fopen("optab.txt","r");
-   intermediate=fopen("intermediate.txt","w");
-   symtab=fopen("symtab.txt","w+");
-   length=fopen("length.txt","w");
-   
-   char label[20],opcode[20],operand[20],symtab_data[20],optab_data[20];
-   int locctr,starting_address,program_length;
-   bool symbol_found,opcode_found;
-   fscanf(source,"%s%s%s",label,opcode,operand);
-   if(strcmp(opcode,"START")==0){
-     starting_address=atoi(operand);
-     locctr=starting_address;
-      fprintf(intermediate,"%d \t\t%s\t\t%s\t%s\n",locctr,label,opcode,operand);
-   }
-   else{
-       locctr=0;
-       }
-   fscanf(source,"%s%s%s",label,opcode,operand);
-   while(strcmp(opcode,"END")!=0){
-       if(strcmp(label,"**")!=0){
-       fprintf(symtab,"%s\t%d\n",label,locctr);
-            symbol_found=false;
+void main() {
+    FILE *source, *optab, *intermediate, *symtab, *length;
+    source = fopen("source.txt", "r");
+    optab = fopen("optab.txt", "r");
+    intermediate = fopen("intermediate.txt", "w");
+    symtab = fopen("symtab.txt", "w+");
+    length = fopen("length.txt", "w");
+
+    if (!source || !optab || !intermediate || !symtab || !length) {
+        printf("Error: One or more files could not be opened.\n");
+        return;
+    }
+
+    char label[20], opcode[20], operand[20], symtab_data[20], optab_data[20];
+    int locctr = 0, starting_address = 0, program_length = 0;
+    bool symbol_found, opcode_found;
+
+    // Read the first line of the source file
+    fscanf(source, "%s %s %s", label, opcode, operand);
+    if (strcmp(opcode, "START") == 0) {
+        starting_address = atoi(operand);
+        locctr = starting_address;
+        fprintf(intermediate, "%-8s %-8s %-8s\n", label, opcode, operand); // Align for START directive
+    } else {
+        locctr = 0;
+    }
+    fscanf(source, "%s %s %s", label, opcode, operand);
+
+    while (strcmp(opcode, "END") != 0) {
+        if (strcmp(label, "**") != 0) {    // searching label in symtab
+            symbol_found = false;
             rewind(symtab);
-           while(!feof(symtab)){
-                  fscanf(symtab,"%s",symtab_data);
-                  if(strcmp(symtab_data,label)==0){
-                  //printf("Error Duplicate\n");
-                  symbol_found=true;
-                  }
-                  }
-             if(symbol_found==false){
-                 fprintf(symtab,"%s\t%d\n",label,locctr);
-                 }
-                 }
-          opcode_found=false;
-          rewind(optab);
-          while(!feof(optab)){
-            fscanf(optab,"%s",optab_data);
-                if(strcmp(optab_data,opcode)==0){
-                  opcode_found=true;
-                  }
-                  }
-                  
-            fprintf(intermediate,"%d\t",locctr);
-            if(opcode_found==true){ 
-                locctr+=3;
+            while (fscanf(symtab, "%s", symtab_data) ==  1) {
+                if (strcmp(symtab_data, label) == 0) {
+                    printf("Error: Duplicate symbol found: %s\n", label);
+                    symbol_found = true;
+                    break;
                 }
-             else if(strcmp(opcode,"WORD")==0)
-                    locctr+=3;
-              else if(strcmp(opcode,"RESW")==0)
-                    locctr+=3*atoi(operand);
-              else if(strcmp(opcode,"RESB")==0)
-                    locctr+= atoi(operand);
-              else if(strcmp(opcode,"BYTE")==0)
-                    locctr=locctr+strlen(operand)-3;
-              else
-                     printf("Error....inavlid opcode\n");
-              fprintf(intermediate,"%s\t%s\t%s\n",label,opcode,operand);
-              fscanf(source,"%s%s%s",label,opcode,operand);
-              }
-               fprintf(intermediate,"%d \t\t%s\t\t%s\t%s\n",locctr,label,opcode,operand);
-               program_length=locctr-starting_address;
-               printf("Program Lengthn %d \n",program_length);
-               fprintf(length,"%d",program_length);
-               
-               fclose(intermediate);
-               
-               fclose(symtab);
-               fclose(optab);
-               fclose(source);
-               fclose(length);
-               }
-                    
-   
+            }
+            if (!symbol_found) {
+                 fprintf(symtab, "%s\t%d\n", label, locctr);
+            }
+        }
 
-   
-   
+        opcode_found = false;
+        rewind(optab);
+        while (fscanf(optab, "%s", optab_data) == 1) {
+            if (strcmp(optab_data, opcode) == 0) {
+                opcode_found = true;
+                break;
+            }
+        }
+
+        // Write formatted output to intermediate file with consistent alignment
+        fprintf(intermediate, "%-8d %-8s %-8s %-8s\n", locctr, label, opcode, operand);
+
+        // Adjust location counter based on opcode
+        if (opcode_found) {
+            locctr += 3;
+        } else if (strcmp(opcode, "WORD") == 0) {
+            locctr += 3;
+        } else if (strcmp(opcode, "RESW") == 0) {
+            locctr += 3 * atoi(operand);
+        } else if (strcmp(opcode, "RESB") == 0) {
+            locctr += atoi(operand);
+        } else if (strcmp(opcode, "BYTE") == 0) {
+            locctr += strlen(operand) - 3;
+        } else {
+            printf("Error: Invalid operation code: %s\n", opcode);
+        }
+
+        // Read the next line of the source file
+        fscanf(source, "%s %s %s", label, opcode, operand);
+    }
+
+    // Final line for END directive
+    fprintf(intermediate, "%-8d %-8s %-8s %-8s\n", locctr, label, opcode, operand);
+    program_length = locctr - starting_address;
+    printf("Program length: %d\n", program_length);
+
+    fclose(source);
+    fclose(optab);
+    fclose(intermediate);
+    fclose(symtab);
+    fclose(length);
+}
